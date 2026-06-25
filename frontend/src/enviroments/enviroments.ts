a6a905ebdfb7fba013207f318418b3d912c3ac4e
bell77-t <alexcueto41@gmail.com>
2026-06-25 08:32:30 -0500

    envirioment

--- frontend/src/enviroments/enviroments.ts
+++ frontend/src/enviroments/enviroments.ts
@@ -3,7 +3,7 @@
   firebase: {
     apiKey: "AIzaSyDUdAYwKZMCt3wP3Ypu3sxl1ci-aVFjFjQ",
     authDomain: "asistenteai-e3530.firebaseapp.com",
-    databaseURL: "https://asistenteai-e3530-default-rtdb.firebaseio.com",
+    databaseURL: "https://asistente-ai-1.onrender.com",
     projectId: "asistenteai-e3530",
     storageBucket: "asistenteai-e3530.firebasestorage.app",
     messagingSenderId: "495679871797",
