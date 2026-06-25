04be39f64632edf3fae8a4142e8bc9e4a0c7b6ea
bell77-t <alexcueto41@gmail.com>
2026-06-24 19:37:20 -0500

    Actualizacion Ai

--- frontend/src/enviroments/enviroments.ts
+++ frontend/src/enviroments/enviroments.ts
@@ -1,10 +1,9 @@
-export const environment = {
-  firebase: {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_AUTH_DOMAIN",
-    projectId: "TU_PROJECT_ID",
-    storageBucket: "TU_STORAGE_BUCKET",
-    messagingSenderId: "TU_MESSAGING_SENDER_ID",
-    appId: "TU_APP_ID"
-  }
+const firebaseConfig = {
+  apiKey: "AIzaSyDUdAYwKZMCt3wP3Ypu3sxl1ci-aVFjFjQ",
+  authDomain: "asistenteai-e3530.firebaseapp.com",
+  databaseURL: "https://asistenteai-e3530-default-rtdb.firebaseio.com",
+  projectId: "asistenteai-e3530",
+  storageBucket: "asistenteai-e3530.firebasestorage.app",
+  messagingSenderId: "495679871797",
+  appId: "1:495679871797:web:16e95d424b9c7baba54a7c"
 };
